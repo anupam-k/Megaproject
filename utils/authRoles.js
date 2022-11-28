31a20859e28a463b53229775bf865da275d01680
Anupam Kumar Krishnan <91872149+anupam-k@users.noreply.github.com>
2022-11-28 03:55:38 +0000

    Start of Mega Project Backend

---
+++ utils/authRoles.js
@@ -0,0 +1,11 @@
+//an object named "AuthRoles"
+// consisting of key value pairs
+// here we are kind of creating "ENUMS"
+
+const AuthRoles = {
+    ADMIN: "ADMIN",
+    MODERATOR: "MODERATOR",
+    USER: "USER"
+}
+
+export default AuthRoles;
