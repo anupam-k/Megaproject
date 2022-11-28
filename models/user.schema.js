40d6a8af4160aa86edbfdaf243dd8c60a4300364
Anupam Kumar Krishnan <91872149+anupam-k@users.noreply.github.com>
2022-11-28 04:38:12 +0000

    Hooks for User Schema

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import AuthRoles from '../utils/authRoles';
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = mongoose.Schema(
     {
@@ -31,5 +34,12 @@ const userSchema = mongoose.Schema(
         timestamps: true
     }
 );
+
+// Challenge 1 - Encrypt Password 
+userSchema.pre("save", async function(next){
+    if(!this.modified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
 
 export default mongoose.model("User", userSchema)
